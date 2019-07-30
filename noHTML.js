719ebd9330d1c8bffb2be5997e5811c72a449630
Annthorn36 <53320751+Annthorn36@users.noreply.github.com>
2019-07-29 20:55:54 -0500

    noHTML.js
    In class saving this file for practice

---
+++ noHTML.js
@@ -0,0 +1,20 @@
+var addingNumbers=23+45;
+console.log(addingNumbers);
+
+var sketch=36;
+console.log(sketch);
+
+console.log(1063* 968/204 % 2)
+sketch={"Name":"Peter ConttonTail","Age": 23
+
+}
+
+
+var boo1=true;
+var scholl=false;
+console.log(String(boo1));
+console.log(String(scholl))
+
+
+var a = 7.0;
+console.log(String(a));
